f5acdcb6b999ca53714e7daa79d63276a0587793
alefra88 <efra.rlz@gmail.com>
2023-02-24 03:27:08 -0600

    background

---
+++ apps/app.js
@@ -0,0 +1,6 @@
+window.addEventListener("load", () => {
+  const $canvas = document.getElementById("canvas1"),
+    ctx = $canvas.getContext("2d");
+  $canvas.width = 1280;
+  $canvas.height = 720;
+});
